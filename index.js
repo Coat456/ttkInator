0ab7f0fc6cb54ce7eec16de39d2b1e4b4675c82f
Coat456 <daimiangouge@gmail.com>
2024-06-04 18:06:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,552 +1,154 @@
-let allgears = [];
-let gear = [];
-let checkNull = [];
-let checkZero = [];
-let dGear=[];
-let alldGear=[];
-let name;
-let RPM;
-let Dmg;
-let lowerBodyDmg = 0;
-let headDmg = 0;
-let upLegDmg = 0;
-let health = 300;
-let chooser;
-let maxLowerBodyshot;
-let maxHeadshot;
-let maxBodyshot;
-let maxUpLegShot;
-let mHealth = 300;
-var numHeadshot = 0;
-var numLowerBodyshot = 0;
-var numBodyshot = 0;
-var numUpLegshot =0;
-let ttkD;
-let firstCalc = false;
-var thisGear
+<!DOCTYPE html>
+<html lang="en">
+<title>Warzone TTK Calculator</title>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="ttk, Time to kill, warzone, call of duty, cod, wz, calculator, body shot, head shot, leg shot">
+    <meta name="description" content="Find the TTK of old, new, or updated COD Guns before anyone else with TTKinator . Calculate TTK and DTTK easily with this tool.">
+    <meta property="og:title" content="TTK Inator" />
+    <meta property="og:type" content="website" />
+    <!-- <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" /> -->
+    <meta property="og:image" content=Untitled_Export_V1.jpeg />
+    <link rel="icon" type="image/x-icon" href="Untitled_Export_V1_1.ico" />
+
+    <link rel="stylesheet" href="mystyle.css">
+</head>
+
+<body>
+
+    <div id="gearListContainer">
+
+        <div class="gList"> 
+            <h3 class="list-title" onclick="toggleActive('savedGearList')">Default Guns &#x25B6;</h3>
+            <div class="InnerContainer">
+            <label for="searchInput">Search For Guns:</label>
+
+            <input id="searchInput" onchange="filterNames()">
+
+        <ul  class="innerList" id="savedGearList">
+      
+
+        </ul>
+    </div>
+    </div>
+    <div class="gList">
+        <h3 class="list-title" onclick="toggleActive('gearList')">Your Guns &#x25B6; </h3>
+        <div class="InnerContainer">
+        <ul class="innerList" id="gearList"></ul>
+        </div>
+    </div>
+    </div>
+    <div class="container">
+        <div class="section" id="section1">
 
 
-function listDefault(){
-    dPushGear("BP50", 33, 30 ,28, 28, 857.1)
-    dPushGear("Bal-27", 30, 30, 28,28, 697.7)
-    dPushGear("SVA 545",40,31,31,29,681.8)
-    dPushGear("RAM-7",33,28,28,25,750)
-    dPushGear("MTZ-556",32,29,29,27,810.8)
-    dPushGear("Holger 556",49,38,38,32,588.2)
-    dPushGear("MCW", 36,30,28,28,714.3)
-    dPushGear("SOA SUBVERTER", 40,39,34,34,697.7)
-    dPushGear("BAS-B", 42,36,36,30,600)
-    dPushGear("Sidewinder", 84,60,60,57,375)
-    dPushGear("MTZ-762",58,49,49,45,540.5)
-    dPushGear("FJX HORUS",39,27,25,25,983.6)
-    dPushGear("RAM-9", 32,29,29,27,923.1)
-    dPushGear("AMR9",35,29,27,27,833.3)
-    dPushGear("Rival-9",33,28,28,26,909.1)
-    dPushGear("HRM-9",32,29,28,28,882.4)
-    dPushGear("Striker 9", 37,31,31,29,857.1)
-    dPushGear("Striker", 46,39,36,36,645.2)
-    dPushGear("WSP-9",54,46,42,42,599.5)
-    dPushGear("WSP Swarm", 29,27,27,24,1090.9)
-    dPushGear("TAQ EVOLVERE",52,48,44,40,499.8)
-    dPushGear("BRUEN MK9",35,28,26,26,759.1)
-    dPushGear("PULEMYOT 762",48,44,40,40,491.6)
-    dPushGear("DG-58 LSW", 40,31,31,29,705.9)
-    dPushGear("TAQ ERADICATOR", 36,33,33,30,857.1)
-    dPushGear("Holger 26",33,30,30,28,705.9)
+    <h1>TTK</h1>
+    <p>Find the TTK of old, new, or updated COD Guns before anyone else with TTKinator . Calculate TTK and DTTK easily with this tool.</p>
+    <div id="STTK"> 
+    <label for="userInput">Gun Name:</label>
+    <input id="userInput" value="My Gun" onchange="nameFill(this)"> <br> <br>
+    <label for="userInput1">RPM:</label>
+    <input id="userInput1" value="0" onchange="rpmFill(this)"> <br> <br>
+    <label for="userInput2">Upper Torso Damage:</label>
+    <input id="userInput2" value="0" onchange="bDmgFill(this)"> <br> <br>
 
 
-   
+
+    <button id="submit" onclick="logGun()">Save Gun</button>
+
+
+    <label for="showTTK">TTK:</label>
+    <input id="showTTK" readonly value="0">
+    <button onclick="toggleVisible('section2')" >Click Here For Differential TTK (TTK for a mix of Lower body, body, and head shots)</button>
+
+    </div>
+
+        </div>
+
+
+        <div class="section" id="section2" >
+
+    <h2>Differential TTK</h2>
+
+    <div id="DTTK">
+
+        <label for="userInput3">Gun Lower Body Damage</label>
+        <input id="userInput3" value="0" onchange="textFilledLBD(this)"> <br> <br>
+        <label for="userInput4">Gun Headshot Damage</label>
+        <input id="userInput4" value="0" onchange="textFilledHSD(this)"> <br> <br>
+        <label for="userInput5">Gun Upper Leg Damage</label>
+        <input id="userInput5" value="0" onchange="textFilledULD(this)"> <br> <br>
+
+        <label for="bs">Number of Upper Body Shots:</label>
+
+        <select name="bs" id="bs" onchange="gunTTKD()">
+        </select>
+
+        <label for="lbs">Number of Lower Body Shots:</label>
+
+        <select name="lbs" id="lbs" onchange="gunTTKD()">
+        </select>
+
+        <label for="hss">Number of Head Shots:</label>
+
+        <select name="hss" id="hss" onchange="gunTTKD()">
+        </select>
+
+        <label for="uls">Number of Upper Leg Shots:</label>
+
+        <select name="uls" id="uls" onchange="gunTTKD()">
+        </select>
+        <label for="showDTTK">DTTK:</label>
+
+        <input id="showDTTK" readonly> <br><br>
+
+        <button id="submit2" onclick="getTTKD()">Submit</button>
+        <label for="mHealth">Enemy Health </label>
+
+        <input id="mHealth" readonly><input>
+
+    </div>
+        </div>
+    </div>
     
+    <footer>
+        <div class="contact-info">
+            <h3>Contact Information</h3>
+            <p>Email: ttkinator@gmail.com</p>
+        </div>
+    </footer>
+    <script src="index.js"> </script>
+    <script>
+        section2 = document.getElementById('section2');
+        section2.style.display = "none";
+        CGL = document.getElementById("gearList");
+        showTTK = document.getElementById("showTTK");
+        cName = document.getElementById("userInput")
+        crpm = document.getElementById("userInput1");
+        cbDmg = document.getElementById("userInput2");
+        clbDmg = document.getElementById("userInput3")
+        chsDmg = document.getElementById("userInput4")
+        culDmg = document.getElementById("userInput5")
+        
 
-    for(i=0; i<alldGear.length;i++){
+        showmHealth = document.getElementById("mHealth");
+        showDTTK = document.getElementById("showDTTK");
+        gearList = document.getElementById("gearList");
+         sga = document.getElementById("savedGearList");
+         listDefault();
 
-        var li = document.createElement("li");
-        var b = document.createElement("button");
-        var onClk = document.createAttribute("onclick");
-        var Value = document.createAttribute("value");
-        Value.value = i;
-        var bClass = document.createAttribute("class");
-        bClass.value= "listButton"
-        onClk.value = "loadDefault(this)";
-        b.setAttributeNode(bClass);
-
-        b.setAttributeNode(onClk);
-        b.setAttributeNode(Value);
-        b.appendChild(document.createTextNode(alldGear[i][0]));
-        li.appendChild(b);
-        sga.appendChild(li);
-    }
-    
-}
-function filterNames() {
-    const input = document.getElementById('searchInput');
-    const filter = input.value.toLowerCase();
-    const ul = document.getElementById('savedGearList');
-    const li = ul.getElementsByTagName('li');
-
-    for (let i = 0; i < li.length; i++) {
-        const txtValue = li[i].textContent || li[i].innerText;
-        if (txtValue.toLowerCase().indexOf(filter) > -1) {
-            li[i].style.display = '';
-        } else {
-            li[i].style.display = 'none';
+    </script>
+    <script type="application/ld+json">
+        {
+          "@context": "https://schema.org",
+          "@type": "WebPage",
+          "name": "TTKinator",
+          "description": "Find the TTK of old, new, or updated COD Guns before anyone else with TTKinator . Calculate TTK and DTTK easily with our tool."
         }
-    }
-}
-function logGun() {
-    checkNull = [this.name]
-    checkZero = [this.Dmg, this.RPM]
-    console.log(valuesNull(checkNull));
+        </script>
+        
+</body>
 
-    if (valuesZero(checkZero) || !firstCalc || valuesNull(checkNull)) {
-        alert("Please Do Not Leave Anything Blank");
-    }else{
-        console.log(gearExists(this.name));
-    if(gearExists(this.name).toString()=="false"&& this.name.toString().length<15){
-    pushGear();
-    var li = document.createElement("li");
-    var b = document.createElement("button");
-    var bClass = document.createAttribute("class");
-    bClass.value= "listButton"
-    var onClk = document.createAttribute("onclick");
-    var Value = document.createAttribute("value");
-    var v = gearID();
-    Value.value = v;
-    onClk.value = "loadGear(this)";
-    b.setAttributeNode(onClk);
-    b.setAttributeNode(bClass);
-    b.setAttributeNode(Value);
-    b.appendChild(document.createTextNode(AgetGearName(allgears.length - 1)));
-    li.appendChild(b);
-    gearList.appendChild(li);
-    if(CGL.classList.contains('active')){
-   toggleActive('gearList');
-    }
-    console.log(CGL.classList.contains('active'));
-}else if (this.name.toString().length<15){
-   
-    allgears[gearExists(this.name)][1] =this.RPM;
-    allgears[gearExists(this.name)][2] = this.Dmg;
-    allgears[gearExists(this.name)][3] = this.lowerBodyDmg;
-    allgears[gearExists(this.name)][4] = this.headDmg;
-
-} else {
-    alert("Gun Name Too Long");
-}
-}
-}
-function gearExists(gn){
-
-for(i=0; i<allgears.length; i++){
-    if(AgetGearName(i)==gn){
-        return i;
-   } 
-  } 
-return false;
-}
-function gearID() {
-
-
-    return ((allgears.length - 1));
-}
-function toggleVisible(e) {
-    var x = document.getElementById(e);
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-
-}
-function toggleActive(e){
-    var x = document.getElementById(e);
-
-x.classList.toggle("active")
-
-
-}
-function pushGear() {
-
-    gear = [];
-    gear.push(this.name.toString());
-    gear.push(this.RPM);
-    gear.push(this.Dmg);
-    gear.push(this.lowerBodyDmg);
-    gear.push(this.headDmg);
-    gear.push(this.upLegDmg);
-
-    allgears.push(gear);
-}
-function dPushGear(n,h,d,l,ul,r){
-    dGear =[];
-    dGear.push(n.toString());
-    dGear.push(r);
-    dGear.push(d);
-    dGear.push(l);
-    dGear.push(h);
-    dGear.push(ul)
-
-    alldGear.push(dGear);
-}
-function gearList() {
-
-}
-
-function AgetGearName(i) {
-    return allgears[i][0];
-}
-function AgetGearLbDmg(i) {
-    return allgears[i][3];
-}
-function AgetGearDmg(i) {
-    return allgears[i][2];
-}
-function AgetGearRPM(i) {
-    return allgears[i][1];
-}
-function AgetGearHD(i) {
-    return allgears[i][4];
-}
-function AgetGearUL(i){
-    return allgears[i][5];
-}
-function getGLB() {
-    return gear[3]
-}
-function getGD() {
-    return gear[2]
-}
-function getGRPM() {
-    return gear[1]
-}
-function getGH() {
-    return gear[4]
-}
-function getUL() {
-    return gear[5]
-}
-
-function gunTTK() {
-    firstCalc = true;
-
-    this.upLegDmg = document.querySelector('#userInput5').value
-    this.name = document.querySelector('#userInput').value
-    this.RPM = document.querySelector('#userInput1').value
-    this.Dmg = document.querySelector('#userInput2').value
-    this.lowerBodyDmg = document.querySelector('#userInput3').value
-    this.headDmg = document.querySelector('#userInput4').value
-
-    console.log(valuesZero(checkZero))
-
-    checkZero = [this.Dmg, this.RPM]
-    checkNull = [this.name]
-    console.log(this.Dmg);
-    console.log(this.name);
-    console.log(this.RPM);
-    if (valuesZero(checkZero) || valuesNull(checkNull)) {
-        console.log("hallo");
-
-        alert("Please Do Not Leave Anything Blank");
-    } else {
-        if(ULDF&&HSDF&&LBDF){
-    gunTTKD();
-        }
-
-        let ttk = (((health / this.Dmg + (1 - ((health % this.Dmg) / this.Dmg))) - 1) / (this.RPM / 60)) * 1000;
-        console.log(ttk);
-        showTTK.value = Math.round(ttk).toString()+"ms";
-    }
-}
-function loadGear(ph) {
-     thisGear = ph.value;
-    console.log(ph.value);
-    culDmg.value = AgetGearUL(thisGear);
-    cbDmg.value = AgetGearDmg(thisGear);
-    crpm.value = AgetGearRPM(thisGear);
-    chsDmg.value = AgetGearHD(thisGear);
-    clbDmg.value = AgetGearLbDmg(thisGear);
-    cName.value = AgetGearName(thisGear);
-
-    rpmFill();
-    bDmgFill();
-    nameFill();
-    textFilledHSD(document.querySelector('#userInput4'));
-    textFilledLBD(document.querySelector('#userInput3'));
-    textFilledULD(document.querySelector('#userInpu53'));
-    console.log(crpm.value);
-
-
-    console.log(ph.value);
-}
-function loadDefault(ph){
-     thisGear = ph.value;
-    console.log(ph.value);
-    culDmg.value = alldGear[thisGear][5];
-    cbDmg.value = alldGear[thisGear][2];
-    crpm.value = alldGear[thisGear][1];
-    chsDmg.value = alldGear[thisGear][4];
-    clbDmg.value = alldGear[thisGear][3];
-    cName.value = alldGear[thisGear][0];
-    rpmFill();
-    bDmgFill();
-    nameFill();
-    textFilledHSD(document.querySelector('#userInput4'));
-    textFilledLBD(document.querySelector('#userInput3'));
-    textFilledULD(document.querySelector('#userInput5'));
-
-    addOptions();
-}
-function gunTTKD() {
-
-
-
-    checkZero[this.lowerBodyDmg, this.Dmg, this.headDmg, this.upLegDmg]
-    if (valuesZero(checkZero)) {
-        console.log("hallo");
-        alert("Please Put Values");
-    } else {
-        mHealth = 300;
-        numUpLegshot = document.querySelector('#uls').value
-        numBodyshot = document.querySelector('#bs').value
-        numLowerBodyshot =document.querySelector('#lbs').value
-        numHeadshot = document.querySelector('#hss').value
-        console.log(upLegDmg);
-
-        getmHealth(numBodyshot,this.Dmg);
-        getmHealth(numHeadshot,this.headDmg);
-        getmHealth(numLowerBodyshot,this.lowerBodyDmg);
-        getmHealth(numUpLegshot,this.upLegDmg);
-        console.log(mHealth);
-        getTTKD();
-        addOptions();
-    }
-
-}
-function getmHealth(numShots, dmg){
-    mHealth -=numShots*dmg;
-}
-function valuesNull(x) {
-    for (i = 0; i < x.length; i++) {
-        if (x[i] == null || x[i] < 0 || x[i] == 'undefined' || x[i] == "") {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-
-}
-function valuesZero(x) {
-    for (i = 0; i < x.length; i++) {
-        if (x[i] == 0 || x[i] < 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
-
-function autoCalc() {
-    if (bd && rpmf) {
-        gunTTK();
-            console.log(document.getElementById('userInput1').value)
-    }
-}
-
-let nf = false;
-function nameFill(x) {
-    if(x.value>0){
-    nf = true;
-    }else{
-        nf=false
-    }
-    autoCalc();
-}
-let bd = false;
-function bDmgFill(x) {
-    if(x.value>0){
-        bd = true;
-        }else{
-            bd=false
-        }
-        autoCalc();
-}
-let rpmf = false;
-function rpmFill(x) {
-    if(x.value>0){
-        rpmf = true;
-        }else{
-            rpmf=false
-        }
-        autoCalc();
-}
-
-function getTTKD() {
-    var nbs;
-    var nlbs;
-    var nhs;
-    var nuls;
-    nbs = numBodyshot;
-    nhs = numHeadshot;
-    nlbs = numLowerBodyshot;
-    nuls = numUpLegshot;
-    if (numBodyshot > 0) {
-        nbs = nbs - 1;
-    } else if (numHeadshot > 0) {
-        nhs = nhs - 1;
-    } else if (numLowerBodyshot > 0) {
-        nlbs = nlbs - 1;
-    } else if (numUpLegshot >0){
-        nuls = nuls-1
-    } else {
-        console.log("Error: No Shots");
-    }
-    console.log(numBodyshot);
-    console.log(!isNaN(nhs));
-    console.log(!isNaN(parseInt(nhs)));
-
-    console.log(nbs);
-
-    if (isNaN(parseInt(nbs))) {
-        nbs = 0;
-    }
-    if (isNaN(parseInt(nuls))) {
-        nuls= 0;
-    }
-    if (isNaN(parseInt(nhs))) {
-        nhs = 0;
-    }
-    if (isNaN(parseInt(nlbs))) {
-        nlbs = 0;
-    }
-
-    console.log(!isNaN(parseInt(nhs)));
-    console.log(nbs);
-
-
-    let totalShots = ((nbs) + (nhs) + (nlbs) + (nuls));
-    console.log(parseInt(totalShots));
-    ttkD = totalShots / (this.RPM / 60)
-    console.log(ttkD);
-    if (mHealth <= 0) {
-        showDTTK.value = (ttkD).toString()+"ms";
-        showmHealth.value = "dead"
-    } else {
-        showmHealth.value = mHealth.toString() + "hp left"
-        showDTTK.value = mHealth.toString() + "hp left";
-
-    }
-
-}
-
-
-function updateOptions() {
-    checkZero = [this.Dmg, this.RPM, this.lowerBodyDmg, this.headDmg, this.upLegDmg]
-    checkNull = [this.name]
-    if (valuesZero(checkZero) || valuesNull(checkNull)) {
-        console.log("hallo");
-        alert("Please Put Values");
-    } else {
-        if (mHealth > 0) {
-            maxBodyshot = Math.ceil(mHealth / this.Dmg);
-            maxHeadshot = Math.ceil(mHealth / this.headDmg);
-            maxLowerBodyshot = Math.ceil(mHealth / this.lowerBodyDmg);
-            maxUpLegShot = Math.ceil(mHealth / this.upLegDmg);
-
-        } else {
-            maxUpLegShot = 0;
-            maxLowerBodyshot = 0;
-            maxHeadshot = 0;
-            maxBodyshot = 0;
-        }
-        console.log(mHealth);
-        console.log(mHealth / this.Dmg);
-        console.log(maxBodyshot);
-
-    }
-}
-function healthDepleted() {
-    return mHealth <= 0;
-}
-var l;
-function removeOption(list){
-   l= list.options.length-1
-   for(i=l;i>=0;i--){
-    list.remove(i);
-   }
-}
-function includeOptions(list, numShot, maxShot){
-    option = document.createElement("option");
-    option.text = numShot;
-    list.add(option);
-    for (j = 0; (j <= maxShot) || j < numShot; j++) {
-        option = document.createElement("option");
-        option.text = j;
-        if (j != numShot || j == 0) {
-            list.add(option);
-        }
-    }
-}
-function addOptions() {
-    uls = document.getElementById("uls")
-    bs = document.getElementById("bs");
-    lbs = document.getElementById("lbs");
-    hss = document.getElementById("hss");
-    var option = document.createElement("option");
-
-    removeOption(bs)
-    removeOption(uls)
-    removeOption(lbs)
-    removeOption(hss)
-
-   
-    updateOptions();
-    includeOptions(bs, numBodyshot, maxBodyshot)
-    includeOptions(lbs, numLowerBodyshot, maxLowerBodyshot)
-    includeOptions(hss, numHeadshot, maxHeadshot)
-    includeOptions(uls, numUpLegshot, maxUpLegShot)
-
-    
-}
-let LBDF = false;
-let HSDF = false;
-let ULDF = false;
-
-
-function textFilledLBD(lbd) {
-    console.log("fatMonkeyDookie");
-    if (lbd.value > 0) {
-        LBDF = true
-    } else{
-        LBDF = false
-    }
-        logDTTK()
-}
-function textFilledHSD(hsd) {
-
-    if (hsd.value > 0) {
-        HSDF = true
-    } else{
-        HSDF = false
-    }
-        logDTTK()
-}
-function textFilledULD(uld) {
-
-    if (uld.value > 0) {
-        ULDF = true
-    } else{
-        ULDF = false
-    }
-        logDTTK()
-}
-function logDTTK(){
-    if (LBDF && HSDF && ULDF) {
-        gunTTK();
-        gunTTKD();
-
-    }
-}
-function test(t) {
-    console.log(t.value);
-}
+</html>
